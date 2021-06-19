d998cc22339492124132682ffc47824fd8ed6a65
natbai1106 <natbai1106@gmail.com>
2021-06-19 16:21:36 -0600

    Agregar archivos de proyecto.

---
+++ RegistroContacto/RegistroContacto/Informacion.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RegistroContacto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Informacion : ContentPage
+    { 
+        public Informacion(String nombre, String apellido, String edad, String correo)
+        {
+            InitializeComponent();
+            infnombre.Text = "Nombre: " + nombre + " " + apellido;
+            infedad.Text = "Edad: " + edad;
+            infcorreo.Text = "Correo: " + correo;
+        }
+    }
+}
---
+++ RegistroContacto/RegistroContacto/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RegistroContacto
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void enviarInformacion_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Informacion(nombreUsuario.Text, apellidoUsuario.Text,edad.Text,correo.Text));
+        }
+    }
+}
